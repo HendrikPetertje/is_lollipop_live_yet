2b05f4d57d090c9ebfd203306116890c38cf16a7
Peter van der Meulen <amonfog@gmail.com>
2014-11-12 19:01:02 +0100

    Updated main file to give a ring when more then 2 items with content '5.0' are found

--- google_script.rb
+++ google_script.rb
@@ -4,10 +4,7 @@ require 'nikkou'
 push_send = false
 while true
   google_site = Nokogiri::HTML(open('https://developers.google.com/android/nexus/images'))
-  if google_site.search('body').text_includes('5.0').any? || google_site.search('body').text_includes('Lollipop').any? || google_site.search('body').text_includes('lollipop').any?
-
-    # Play a fancy sound (change to aplay to afplay if you are using mac)
-    system("aplay google-alert.wav")
+  if google_site.xpath("//td").text_includes('5.0').count > 2
 
     # send a pushbullet out notifying all workstations about the patch
     if push_send == false
@@ -17,5 +14,5 @@ while true
   else
     puts Time.now.strftime("No update at %H:%M")
   end
-  sleep 600
+  sleep 120
 end
