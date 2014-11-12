64c657107da7eca4ecf80a7263bcaeb717316303
Peter van der Meulen <amonfog@gmail.com>
2014-11-12 19:03:20 +0100

    Updating send push with channels

--- send_push.sh
+++ send_push.sh
@@ -1,2 +1,2 @@
 # change <YOUR_API_CODE_HERE> with an API code from https://www.pushbullet.com/account
-curl -u <YOUR_API_CODE_HERE>: -X POST https://api.pushbullet.com/v2/pushes --header 'Content-Type: application/json' --data-binary '{"type": "note", "title": "Android Lollipop", "body": "Android Lollipop Could be live. Get to your nearest console now!"}'
+curl -u <YOUR_API_CODE_HERE>: -X POST https://api.pushbullet.com/v2/pushes --header 'Content-Type: application/json' --data-binary '{"type": "note", "channel_tag": "<your_channel_here>", "title": "Android Lollipop", "body": "New text with content 5.0 is posted to the Lollipop Factory images page"}'
